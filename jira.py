89f21fef93cc42f93c048bbb17bf7bd334492dfb
David Brady <david.brady@acimacredit.com>
2022-08-22 16:22:23 +0000

    Extract bad ticket object and collection code to BadTicket, BadTickets classes

--- jira.py
+++ jira.py
@@ -1,17 +1,48 @@
 #!/usr/bin/env python
-# jira.py - my sketch/burn/monkeypatch copy of our jira ticket reader
+# jira.py - Dump out list of eligible next JIRA tickets. This is my sketch/burn/monkeypatch copy of DS Team's jira ticket reader.
+# RUN THIS WITH next-ticket (or don't, I'm not your real Dad)
 import argparse
 
 from dataservices import jira
 import json
 import os
 
+class BadTicket:
+    """
+    BadTicket
+    number - jira case number, integer, e.g. "DS-1406" would be int(1406)
+    comment - reason ticket should be skipped, e.g. "Blocked until finance connection exists"
+    ticket_id - jira ticket id as a string, e.g. "DS-1406"
+    """
+    def __init__(self, id, comment=""):
+        self.id = id
+        self.comment = comment
+        self.ticket_id = f"DS-{id}"
+
+    def __str__(self):
+        f"DS-{self.id}"
+
+
+class BadTickets:
+    def __init__(self):
+        try:
+            with open(os.path.expanduser("~/jira-bad-tickets.json")) as file:
+                tickets = json.loads(file.read())["tickets"]
+                self.tickets = [BadTicket(ticket["id"], ticket.get("comment", None)) for ticket in tickets]
+        except FileNotFoundError:
+            self.tickets = []
+
+    def ids(self):
+        return [ticket.ticket_id for ticket in self.tickets]
+
+
 def get_bad_tickets():
     try:
         with open(os.path.expanduser("~/jira-bad-tickets.json")) as file:
-            return json.loads(file.read())
+            return json.loads(file.read())["tickets"]
     except FileNotFoundError:
         return []
+
 
 def find_ready_for_dev():
     # ticket_number = input('Epic ticket number with children tickets (ex: DS-879): ')
@@ -25,12 +56,15 @@ def find_ready_for_dev():
     start_at = 0
     dave_count = 0
     dave_max = 10
-    bad_tickets = get_bad_tickets()
-    bad_tickets = [f"DS-{ticket_id}" for ticket_id in bad_tickets]
-    print(bad_tickets)
+    bad_tickets = BadTickets()
+    bad_ticket_ids = bad_tickets.ids()
+    print(bad_ticket_ids)
+
     while not finished:
         tickets = jira_client.find_issue_by_epic_id(ticket_number, fields=fields, start_at=start_at)
+        print(f"FOUND {len(tickets)} TICKETS!")
         for ticket in tickets['issues']:
+            # print(ticket['key'])
             ready = True
             issuelinks = ticket['fields']['issuelinks']
             for issue in issuelinks:
@@ -38,8 +72,14 @@ def find_ready_for_dev():
                     if issue['inwardIssue']['fields']['status']['statusCategory']['name'] != 'Done' or ticket['fields']['status']['statusCategory']['name'] != 'To Do':
                         ready = False
 
-            if ticket["key"] in bad_tickets:
-                print(f"Skipping {ticket['key']}")
+            # if ticket["key"] in bad_tickets <-- can we hijack the "in" operator?
+            #
+            # bad_ticket = bad_tickets.find(ticket["key"])
+            # if bad_ticket is not None:
+            #     print(f"skipping ticket {bad_ticket.id} because {bad_ticket.reason}")
+            if ticket["key"] in bad_ticket_ids:
+                # ticket = find_ticket(ticket["key"], bad_tickets)
+                print(f'Skipping {ticket["key"]}: \033[2;37mhttps://acima.atlassian.net/browse/{ticket["key"]}\033[0m')
                 continue
 
             if ready:
@@ -61,7 +101,8 @@ if __name__ == '__main__':
         '--ready_for_dev', action='store_true', help='Find tickets that are ready for development', required=False)
     args = parser.parse_args()
 
-    ready_for_dev = args.ready_for_dev or False
+    # ready_for_dev = args.ready_for_dev or False
+    ready_for_dev = True
 
     if ready_for_dev:
         find_ready_for_dev()
