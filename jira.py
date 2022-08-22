f71e28272949a06029e99d20c14e77a7c86b5b8e
David Brady <david.brady@acimacredit.com>
2022-08-22 18:50:35 +0000

    Extract BadTickets to own class file

--- jira.py
+++ jira.py
@@ -3,38 +3,10 @@
 # RUN THIS WITH next-ticket (or don't, I'm not your real Dad)
 import argparse
 
+import bad_tickets
 from dataservices import jira
 import json
 import os
-
-class BadTicket:
-    """
-    BadTicket
-    number - jira case number, integer, e.g. "DS-1406" would be int(1406)
-    comment - reason ticket should be skipped, e.g. "Blocked until finance connection exists"
-    ticket_id - jira ticket id as a string, e.g. "DS-1406"
-    """
-    def __init__(self, id, comment=""):
-        self.id = id
-        self.comment = comment
-        self.ticket_id = f"DS-{id}"
-
-    def __str__(self):
-        f"DS-{self.id}"
-
-
-class BadTickets:
-    def __init__(self):
-        try:
-            with open(os.path.expanduser("~/jira-bad-tickets.json")) as file:
-                tickets = json.loads(file.read())["tickets"]
-                self.tickets = [BadTicket(ticket["id"], ticket.get("comment", None)) for ticket in tickets]
-        except FileNotFoundError:
-            self.tickets = []
-
-    def ids(self):
-        return [ticket.ticket_id for ticket in self.tickets]
-
 
 def get_bad_tickets():
     try:
